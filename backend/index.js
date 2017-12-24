4e06b436306f47d6b4663b85f05d6436b3137b47
Nurdaulet <b.nurdaulet.b@gmail.com>
2017-12-24 13:35:44 +0600

    feat: add dashboard

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,52 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
+var ParseDashboard = require('parse-dashboard');
 var path = require('path');
+
+var appId = 'vm5lVClO04AYuSxqmcS0wpFjooVNuZbk9X6uN0xYrpo=';
+var masterKey = 'uE2tekbwMzz4g0PVxFgjTeEsyEdWvJVCtOS0bSim+PA=';
+var fileKey = '0xMt9m/1ekpIP6Y7vo/FuSZ8xHmpWmV9FdhYbK6GuOo=';
+var serverURL =
+  'http://ec2-34-251-88-37.eu-west-1.compute.amazonaws.com:1337/parse';
 
 var api = new ParseServer({
   databaseURI: 'mongodb://localhost:27017/prod',
   cloud: __dirname + '/cloud/main.js',
-  appId: 'vm5lVClO04AYuSxqmcS0wpFjooVNuZbk9X6uN0xYrpo=',
-  masterKey: 'uE2tekbwMzz4g0PVxFgjTeEsyEdWvJVCtOS0bSim+PA=',
-  serverURL:
-    'http://ec2-34-251-88-37.eu-west-1.compute.amazonaws.com:1337/parse',
+  appId: appId,
+  masterKey: masterKey,
+  // fileKey: fileKey,
+  serverURL: serverURL,
 });
+
+var allowInsecureHTTP = true;
+var dashboard = new ParseDashboard(
+  {
+    apps: [
+      {
+        appId: appId,
+        masterKey: masterKey,
+        serverURL: serverURL,
+        // fileKey: fileKey,
+        appName: 'TeaTimes',
+      },
+    ],
+    users: [
+      {
+        user: 'tea',
+        pass: 'pass',
+      },
+    ],
+  },
+  allowInsecureHTTP,
+);
 
 var app = express();
 
 app.use('/public', express.static(path.join(__dirname, '/public')));
 
-var mountPath = '/parse';
-app.use(mountPath, api);
+app.use('/parse', api);
+
+app.use('/dashboard', dashboard);
 
 app.get('/', function(req, res) {
   res
