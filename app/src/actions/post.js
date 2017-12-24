574030d81161e50b3ebee340bf307203ffcb6ba5
Zhandos Nurakhmetov <zhandos.nurakhmetov@mail.ru>
2017-12-24 20:43:25 +0600

    feat: add firebase request to action

--- app/src/actions/post.js
+++ app/src/actions/post.js
@@ -1,12 +1,39 @@
+import * as firebase from 'firebase';
 import { FETCH_POSTS } from './types';
 
 export function fetchPosts() {
-  return ({
-    type: FETCH_POSTS,
-    posts: {
-      post1: {
-        title: 'First post'
-      }
-    }
-  });
+  return (dispatch) => {
+    const posts = firebase.database().ref('/posts');
+    posts.on('value', (snapshot) => {
+      console.log(snapshot);
+      dispatch({ type: FETCH_POSTS, posts: snapshot.val() });
+    });
+  };
+
+    // return ({
+    //   type: FETCH_POSTS,
+    //   posts: {
+    //     post1: {
+    //       title: 'First post'
+    //     }
+    //   }
+    // });
 }
+
+// export const changeMode = ({ mode }) => {
+//   const { currentUser } = firebase.auth();
+//   const newMode = (mode === DEFAULT) ? MASTER : DEFAULT;
+//   loaderHandler.showLoader('Requesting...');
+//   return (dispatch) => {
+//     firebase.database().ref(`/Users/${currentUser.uid}/`)
+//     .update({ mode: newMode })
+//     .then(() => {
+//       loaderHandler.hideLoader();
+//       dispatch({ type: CHANGE_MODE, payload: { mode: newMode } });
+//     })
+//     .catch(() => {
+//       console.log('ERROR updating mode...');
+//       loaderHandler.hideLoader();
+//     });
+//   };
+// };
