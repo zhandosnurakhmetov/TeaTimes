1184f8a66242e4844bc782b1974d35989a4b2c2a
Zhandos Nurakhmetov <zhandos.nurakhmetov@mail.ru>
2017-12-26 15:14:20 +0600

    feat(DetailedStory): finish drawing

--- app/src/views/stories/AudioPlayer.js
+++ app/src/views/stories/AudioPlayer.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Image, Text } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import constants from '../../constants';
+
+const { fontWeight } = constants;
 
 export default class AudioPlayer extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <View />
-        <Text>The Fox and The Crow</Text>
+        <Icon name="play-arrow" style={styles.icon} size={30} />
+        <Text style={styles.title}>The Fox and The Crow</Text>
       </View>
     );
   }
@@ -14,7 +18,21 @@ export default class AudioPlayer extends Component {
 
 const styles = StyleSheet.create({
   container: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
     height: 50,
     backgroundColor: '#D7CCC8'
+  },
+  icon: {
+    marginLeft: 15
+  },
+  title: {
+    fontSize: 15,
+    color: 'black',
+    fontFamily: 'Avenir',
+    fontWeight: fontWeight.medium,
+    backgroundColor: 'transparent',
+    marginLeft: 20
   }
 });
